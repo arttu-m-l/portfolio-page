42ba800e8ca1b356e2d579d77b6ecb8a813fe7ed
Arttu Lehtola <arttu.m.lehtola@gmail.com>
2023-08-04 19:50:16 +0300

    Publish

---
+++ js/sticky.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {myFunction()};
+var nav = document.getElementById("nav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+} 
